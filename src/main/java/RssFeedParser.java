53189f3a95ba5bf0a8ba5f5ff498a59efef882f4
balvinderz <balvindersi2@gmail.com>
2020-06-09 11:09:39 +0530

    rssitem

--- src/main/java/RssFeedParser.java
+++ src/main/java/RssFeedParser.java
@@ -6,7 +6,10 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.xml.parsers.*;
 
 public class RssFeedParser {
@@ -17,7 +20,8 @@ public class RssFeedParser {
 
     public  static  void main(String[] args)
     {
-        File f = new File("/Users/balvinder/IdeaProjects/rssbot/src/main/java/feed_example.txt");
+        //File f = new File("/Users/balvinder/IdeaProjects/rssbot/src/main/java/feed_example.txt");
+        File f = new File("E:\\rssbot\\src\\main\\java\\feed_example.txt");
         try {
             FileInputStream fis = new FileInputStream(f);
             byte[] data = new byte[(int) f.length()];
@@ -33,6 +37,8 @@ public class RssFeedParser {
                     str.toString().getBytes("UTF-8"));
             Document doc = builder.parse(input);
             NodeList x = doc.getElementsByTagName("item");
+            ArrayList<RssItem> rssItems = new ArrayList<>();
+
             for(int i =0;i<x.getLength(); i++)
             {
                 Node nNode = x.item(i);
@@ -41,24 +47,36 @@ public class RssFeedParser {
                     Element eElement = (Element) nNode;
                    String link =  eElement.getElementsByTagName("link").item(0).getTextContent();
                    String title = eElement.getElementsByTagName("title").item(0).getTextContent();
-                   print(title);
-                   print(link);
+                    Pattern typePattern = Pattern.compile("\\(\\w*\\)");
+                    Matcher m = typePattern.matcher(title);
+                    m.find();
+                    Pattern pricePattern  = Pattern.compile("\\$\\d\\.\\d*");
+                    Matcher p = pricePattern.matcher(title);
+                     p.find();
+                    float price = Float.parseFloat(title.substring(p.start()+1,p.end()));
+                   String type = title.substring(m.start()+1,m.end()-1);
+                   rssItems.add(new RssItem(link,price,type));
                 }
 
             }
-            Element root = doc.getDocumentElement();
+
+            String type = "Edit";
+            float min = 0;
+            float max =4.5f;
+
+            ArrayList<RssItem> filteredList = filterRssList(type,min,max,rssItems);
+            print(rssItems.get(2).type);
 
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        } catch (SAXException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
+
+    }
+
+    private static ArrayList<RssItem> filterRssList(String type, float min, float max, ArrayList<RssItem> rssItems) {
+        return null;
 
     }
 
---
+++ src/main/java/RssItem.java
@@ -0,0 +1,12 @@
+class RssItem{
+    String link;
+    float amount;
+    String type;
+
+    public RssItem(String link, float amount, String type) {
+        this.link = link;
+        this.amount = amount;
+        this.type = type;
+    }
+
+}
