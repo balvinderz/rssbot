8a4d53df21a78f617fa864ec65038dc5133bc0d4
balvinderz <balvindersi2@gmail.com>
2020-06-10 22:35:57 +0530

    temp

--- src/main/java/AnotheTest.java
+++ src/main/java/AnotheTest.java
@@ -20,19 +20,33 @@ public class AnotheTest {
     public  static  void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/balvinder/Desktop/auto_importer/ch");
 
-
         WebDriver driver = new HtmlUnitDriver();
         driver.get("https://gengo.com/");
+
         driver.manage().deleteAllCookies();
         driver.manage().addCookie(new Cookie("myG_myGSession_","610520f34d56769ba3096bfbaee659934c8cad14"));
+        long startTime = System.nanoTime();
 
-        driver.get("http://localhost:8080/Type%20anchor%20-%20not%20clicked%20yet.htm");
+        driver.get("https://gengo.com/t/dashboard/");
+        print(driver.getCurrentUrl());
+
+        driver.get("http://localhost:8000/Type%20anchor%20-%20not%20clicked%20yet.htm");
+        long endTime = System.nanoTime();
+
+        long timeElapsed = endTime - startTime;
+        System.out.println("Execution time in milliseconds : " +
+                timeElapsed / (1000000));
         try{
-            driver.findElement(By.id("start_job_button")).click();
+           WebElement element =  driver.findElement(By.id("start_job_button"));
+           element.click();
+
 
         }
+
         catch (Exception e)
         {
+            e.printStackTrace();;
+
             try {
                 driver.findElement(By.id("start")).click();
             }catch (Exception f)
